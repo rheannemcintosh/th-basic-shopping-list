1acee4e3a14837cbd40d05dff6d6e93649e34b18
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-31 23:46:43 +0100

    Add an item to the shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,9 @@
+shopping_list = []
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("Added {} to the List! List now has {} item.".format(item, len(shopping_list)))
+
 def show_help():
     print("What should we buy today? ")
     print("""
