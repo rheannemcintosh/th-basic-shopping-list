8af4076490631de487cf1c8c371d42a91c12d11d
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-31 23:32:53 +0100

    Allow user to view help for the application

--- shopping_list.py
+++ shopping_list.py
@@ -1,6 +1,16 @@
+def show_help():
+    print("What should we buy today? ")
+    print("""
+        Enter 'DONE' to stop adding items.
+        Enter 'HELP' for this help.
+        """)
+
+show_help()
 
 while True:
     new_item = input('> ')
 
     if new_item == 'DONE':
         break
+    elif new_item == 'HELP':
+        show_help()
