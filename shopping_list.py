cfd28e5c5b43a7418c1533f61be7a74b5435a4e4
rheannemcintosh <rheanne.mcintosh@abdn.ac.uk>
2021-03-31 23:57:59 +0100

    Update shopping_list.py to PEP8 Standards

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,26 @@
-shopping_list = []
+# ---------------------------------------------------------------- #
+#
+# shopping_list.py
+#
+# This is a sample project created to create a simple Python
+# shopping list via the command line. Built in conjuntion with the
+# Python Introducing Lists course at teamtreehouse.com
+#
+# Author:  Rheanne McIntosh <rheanne.mcintosh@outlook.com>
+# Created: November 2020
+#
+# ---------------------------------------------------------------- #
 
+
+# Function to add an item to a list
 def add_to_list(item):
     shopping_list.append(item)
-    print("Added {} to the List! List now has {} item.".format(item, len(shopping_list)))
+    print(
+        "Added {} to the List! List now has {} item."
+        .format(item, len(shopping_list)))
 
+
+# Function to display command line help to the user
 def show_help():
     print("What should we buy today? ")
     print("""
@@ -12,13 +29,21 @@ def show_help():
         Enter 'SHOW' to display the shopping list.
         """)
 
+
+# Function to display the shopping list to the user
 def show_list():
     print("Here is your list:")
     for item in shopping_list:
         print(item)
 
+
+# Declare an empty shopping list
+shopping_list = []
+
+# Display the help when the user first opens the program
 show_help()
 
+# Use a loop for user input
 while True:
     new_item = input('> ')
 
@@ -33,4 +58,5 @@ while True:
 
     add_to_list(new_item)
 
+# Display the final shopping list
 show_list()
